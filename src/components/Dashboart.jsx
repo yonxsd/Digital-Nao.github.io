7a0a739d87a741f6e0b09630ebdc1715e9efb663
Jonatan David Campos Barrera <gemeryonatan077@gmail.com>
2024-04-21 14:39:41 -0600

    modificado

--- src/components/Dashboart.jsx
+++ src/components/Dashboart.jsx
@@ -56,7 +56,7 @@ const Dashboart = () => {
             <div className="stat-figure text-secondary">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
             </div>
-            <div className="stat-title text-2xl font-bold">Page Views</div>
+            <div className="stat-title text-2xl font-bold">Sales</div>
             <div className="stat-value text-secondary">2.6M</div>
             <div className="stat-desc">21% more than last month</div>
           </div>
