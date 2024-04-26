e26b2ef13c933ee44eab6c2d822506458ade516a
Jonatan David Campos Barrera <gemeryonatan077@gmail.com>
2024-04-26 13:22:10 -0600

    cambios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Sidebar from "./layout/Sidebar";
-import { BrowserRouter,Route, Routes } from 'react-router-dom';
-
+{/*import { BrowserRouter,Route, Routes } from 'react-router-dom';*/
+}
 
 
 const App = () => {
   return (
     <div >
        <Sidebar />
-       {/*hello */}
+       {/*hello user  hi how are you */}
         
     </div>
     
